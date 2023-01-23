c6d2096d9ccd9717e9dd0e4d9c01289e281b904f
Sabine Franz <sabine.franz@capgemini.com>
2023-01-23 10:47:47 +0100

    add app insights

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,6 +18,10 @@ provider "azurerm" {
   features {}
 }
 
+locals {
+  my_prefix = "sfranz"
+}
+
 #Get resource group
 data "azurerm_resource_group" "wsdevops" {
   name = var.rg_name
@@ -48,3 +52,39 @@ resource "azurerm_app_service" "website" {
   }
 }
 
+resource "azurerm_log_analytics_workspace" "log" {
+  name                = "${local.my_prefix}-lg-analytics"
+  location            = data.azurerm_resource_group.wsdevops.location
+  resource_group_name = data.azurerm_resource_group.wsdevops.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+resource "azurerm_application_insights" "appi" {
+  name                = "${local.my_prefix}-api"
+  location            = data.azurerm_resource_group.wsdevops.location
+  resource_group_name = data.azurerm_resource_group.wsdevops.name
+  workspace_id        = azurerm_log_analytics_workspace.log.id
+  application_type    = "web"
+}
+
+// Skeleton for linking web app and app insights
+resource "null_resource" "link_monitoring" {
+  provisioner "local-exec" {
+    command = <<EOT
+      // Login to Azure CLI (Linux operating system assumed)
+az webapp config appsettings set --name $web_app_name --resource-group $rg_name --settings APPINSIGHTS_INSTRUMENTATIONKEY=$inst_key APPINSIGHTS_PROFILERFEATURE_VERSION=1.0.0 APPINSIGHTS_SNAPSHOTFEATURE_VERSION=1.0.0 APPLICATIONINSIGHTS_CONNECTION_STRING=$conn_str ApplicationInsightsAgent_EXTENSION_VERSION=~3 DiagnosticServices_EXTENSION_VERSION=~3 InstrumentationEngine_EXTENSION_VERSION=disabled SnapshotDebugger_EXTENSION_VERSION=disabled XDT_MicrosoftApplicationInsights_BaseExtensions=recommended XDT_MicrosoftApplicationInsights_PreemptSdk=disabled      
+    EOT
+    environment = {
+      // Parameters needed to login
+      con_client_id     = var.client_id
+      con_client_secret = var.client_secret
+      con_tenant_id     = var.tenant_id
+      // Parameters needed for linking
+      inst_key          = appi.instrumentation_key
+      conn_str          = appi.connection_string
+      rg_name           = var.rg_name
+      web_app_name      = var.web_app_name
+    }
+  }
+}
